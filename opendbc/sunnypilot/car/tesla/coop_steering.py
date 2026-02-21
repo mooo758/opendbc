bed1653fe98c8a855795c1a06667a9b3a4dbde17
dzid26 <dzidmail@gmail.com>
2026-02-21 06:26:31 +0000

    disable filtering for now

--- opendbc/sunnypilot/car/tesla/coop_steering.py
+++ opendbc/sunnypilot/car/tesla/coop_steering.py
@@ -333,7 +333,7 @@ class CoopSteeringCarController:
     apply_angle = self.resume_steer_desired_rate_limit(lat_active, apply_angle, steeringAngleDegPhaseLead)
 
     if angle_coop_enabled:
-      apply_angle = self.overriding_steer_desired_accel_limit(lat_active, apply_angle, CS.out.vEgo, CS.out.steeringTorque)
+      # apply_angle = self.overriding_steer_desired_accel_limit(lat_active, apply_angle, CS.out.vEgo, CS.out.steeringTorque)
       self.debug_angle_desired_limited = apply_angle #! debug
 
       apply_angle += self.apply_override_angle_combined(lat_active, CS.out.steeringTorque, CS.out.vEgo, VM)
